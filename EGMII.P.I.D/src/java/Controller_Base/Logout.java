7dba57cd19399f4db41cee6533be027e5810b6b5
shermainesy <shermaine_sy@dlsu.edu.ph>
2015-11-02 13:59:45 +0800

    move index

--- EGMII.P.I.D/src/java/Controller_Base/Logout.java
+++ EGMII.P.I.D/src/java/Controller_Base/Logout.java
@@ -1,8 +1,12 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package Controller_Base;
 
 import java.io.IOException;
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletContext;
+import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -11,29 +15,33 @@ import javax.servlet.http.HttpSession;
 
 /**
  *
- * @author Atayan
- * @author Lapidario
- * @author Sy
- * @author Nunez
- *
+ * @author Geraldine
  */
-public abstract class BaseServlet extends HttpServlet {
+public class Logout extends HttpServlet {
 
-    public abstract void servletAction(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException;
-
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+
         HttpSession session = request.getSession();
-        String login = (String) session.getAttribute("successful");
-        if (login.equalsIgnoreCase("successful")) {
-            servletAction(request, response);
-        } else {
-            ServletContext context = getServletContext();
-            RequestDispatcher rd = context.getRequestDispatcher("/index.jsp");
-            rd.forward(request, response);
-        }
+        if(session != null)
+            session.invalidate();
+        
+        request.getRequestDispatcher("/index.jsp").include(request, response);
+
+        out.print("You are successfully logged out!");
+
+        out.close();
 
     }
 
@@ -75,4 +83,5 @@ public abstract class BaseServlet extends HttpServlet {
     public String getServletInfo() {
         return "Short description";
     }// </editor-fold>
+
 }
