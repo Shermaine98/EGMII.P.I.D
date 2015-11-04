7367e1b8e093072f86496e6a849ce81009585a9a
shermainesy <shermaine_sy@dlsu.edu.ph>
2015-11-04 22:41:12 +0800

    Encode Product Serlvet and Table Design

--- EGMII.P.I.D/src/java/Controller_Encode/EncodeProductServlet.java
+++ EGMII.P.I.D/src/java/Controller_Encode/EncodeProductServlet.java
@@ -38,7 +38,7 @@ public class EncodeProductServlet extends BaseServlet {
     @Override
     public void servletAction(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        ProductBM productBM = new ProductBM();
+     //   ProductBM productBM = new ProductBM();
         ArrayList<ProductBM> ProductListBM = new ArrayList<>();
         ArrayList<Product> ProductList = new ArrayList<>();
         ProductDAO productDAO = new ProductDAO();
@@ -46,11 +46,11 @@ public class EncodeProductServlet extends BaseServlet {
         RefSizesDAO refSizeDAO = new RefSizesDAO();
 
         String productName = request.getParameter("productName");
-           System.out.println(productName);
+        System.out.println(productName);
         String productType = request.getParameter("productType");
         String color = request.getParameter("color");
         String inventoryType = request.getParameter("inventoryType");
-      
+        ProductBM productBM = new ProductBM();
         //itemCode   
         String[] itemCode = request.getParameterValues("itemCode");
         String[] itemConsumption = request.getParameterValues("itemConsumption");
@@ -61,15 +61,20 @@ public class EncodeProductServlet extends BaseServlet {
         String[] unitMeasurementfabric = request.getParameterValues("unitMeasurement");
         String[] size = request.getParameterValues("size");
         String[] itemConsumptionfabric = request.getParameterValues("fabricItemConsumption");
+        for (int i = 0; i < itemConsumptionfabric.length; i++) {
+
+            System.out.println("PRINT" + itemConsumptionfabric[i]);
+
+        }
 
         try {
             //get sizes
             productTypeSize = refSizeDAO.GetAllSizes(productType);
-            
+
         } catch (ParseException ex) {
             Logger.getLogger(EncodeProductServlet.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+
         Integer productNumber = 0;
         try {
             productNumber = productDAO.getProductNumber();
@@ -79,9 +84,9 @@ public class EncodeProductServlet extends BaseServlet {
 
         boolean x = false;
 
-   //encode Product    
+        //encode Product    
         for (int i = 0; i < productTypeSize.size(); i++) {
-              Product product = new Product();
+            Product product = new Product();
             if (i == 0 || x == true) {
                 product.setSize(productTypeSize.get(i));
                 product.setItemCode(productNumber);
@@ -89,7 +94,7 @@ public class EncodeProductServlet extends BaseServlet {
                 product.setProductType(productType);
                 product.setColor(color);
                 product.setInventoryType(inventoryType);
-                  System.out.println(productNumber);
+                System.out.println(productNumber);
                 if (productDAO.EncodeProduct(product)) {
                     x = true;
                     ProductList.add(product);
@@ -105,44 +110,74 @@ public class EncodeProductServlet extends BaseServlet {
         //encode ProductBM
         if (x == true) {
             for (int a = 0; a < ProductList.size(); a++) {
-                 System.out.println("1st" + ProductList.get(a).getItemCode()); 
+                System.out.println("1st" + ProductList.get(a).getItemCode());
                 if (a == 0 || x == true) {
                     for (int y = 0; y < itemCode.length; y++) {
                         productBM.setProductID(ProductList.get(a).getItemCode());
-                        System.out.println("itmeCOde"+ProductList.get(a).getItemCode());
+                        System.out.println("itmeCOde" + ProductList.get(a).getItemCode());
                         productBM.setItemCode(Integer.parseInt(itemCode[y]));
                         productBM.setQty(Integer.parseInt(itemConsumption[y]));
+
                         if (productDAO.EncodeProductBM(productBM)) {
-                         x = true;
-                         ProductListBM.add(productBM);
-                    } else {
-                        x = false;
-                    }
+                            x = true;
+                            ProductListBM.add(productBM);
+                        } else {
+                            x = false;
+                        }
                     }
                 }
             }
-            for (int i = 0; i < ProductList.size(); i++) {
-                if (i == 0 || x == true) {
-                    for (int y = 0; y < itemCodefabric.length; y++) {
-                        productBM.setProductID(ProductList.get(i).getItemCode());
-                        productBM.setItemCode(Integer.parseInt(itemCodefabric[y]));
-                        productBM.setQty(Integer.parseInt(itemConsumptionfabric[y]));
-                    
-                    if (productDAO.EncodeProductBM(productBM)) {
-                        x = true;
-                        ProductListBM.add(productBM);
-                    } else {
-                        x = false;
+            
+          for (int i = 0; i < itemCodefabric.length; i++) {
+                productBM.setItemCode(Integer.parseInt(itemCodefabric[i]));
+                for (int y = 0; y < ProductList.size(); y++) {
+                    if (y == 0 || x == true) {
+                        int input = y + (i * ProductList.size());
+                        productBM.setProductID(ProductList.get(y).getItemCode());
+                        productBM.setQty(Integer.parseInt(itemConsumptionfabric[input]));
+                        System.out.println(itemConsumptionfabric[y]);
+                        System.out.println("input: "+ input);
+                        if (productDAO.EncodeProductBM(productBM)) {
+                            x = true;
+                            ProductListBM.add(productBM);
+                        } else {
+                            x = false;
+                        }
                     }
                 }
-                }
-            }
+            }  
+            
+//            int k=0;
+//             for(int z =0; z < itemCodefabric.length;z++){
+//            int itemCodeTrial = Integer.parseInt(itemCodefabric[z]);
+//            for (int i = 0; i < ProductList.size(); i++) {
+//                    for ( int y = k; y < itemConsumptionfabric.length; k++) {
+//                        ProductBM productBM = new ProductBM();
+//                        System.out.print("ENTER" + ProductList.get(i).getItemCode());
+//                        productBM.setProductID(ProductList.get(i).getItemCode());
+//                        productBM.setItemCode(itemCodeTrial);
+//                        productBM.setQty(Integer.parseInt(itemConsumptionfabric[y]));
+//                         System.out.print("number" + ProductList.get(i).getItemCode());
+//                         System.out.print("length" + itemConsumptionfabric.length);
+//                        System.out.print("itemConsumption" + itemConsumptionfabric[y]);
+//                         k=y;
+//                        if (productDAO.EncodeProductBM(productBM)) {
+//                        x = true;
+//                        ProductListBM.add(productBM);
+//                    } else {
+//                        x = false;
+//                    }
+//                    }
+//                    
+//                }
+//            
+//          }
         }
 
         if (x == true) {
             ServletContext context = getServletContext();
             RequestDispatcher rd = context.getRequestDispatcher("/index.jsp");
-            request.setAttribute("productBM", productBM);
+            request.setAttribute("productBM", ProductListBM);
             request.setAttribute("productList", ProductList);
             rd.forward(request, response);
         } else {
